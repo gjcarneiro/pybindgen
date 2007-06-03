d29767462a7e2806be0a805fb6f2fadf7c93fd68
Gustavo J. A. M. Carneiro <gjc@gnome.org>
2007-06-03 19:14:59 +0100

    add some unit tests for the built example

---
+++ examples/footest.py
@@ -0,0 +1,48 @@
+import sys
+import os.path
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                '..', 'build', 'default', 'examples'))
+import foo
+import unittest
+
+
+class TestFoo(unittest.TestCase):
+
+    def test_add_prefix(self):
+        obj = foo.SomeObject("hello ")
+        ln, msg = obj.add_prefix("gjc")
+        self.assertEqual(msg, "hello gjc")
+        self.assertEqual(ln, len("hello gjc"))
+
+    def test_foo(self):
+        f = foo.Foo("hello")
+        self.assertEqual(f.get_datum(), "hello")
+
+    def test_pass_foo_by_value(self):
+        obj = foo.SomeObject("")
+        f = foo.Foo("hello")
+        obj.set_foo_value(f)
+
+        f2 = obj.get_foo_value()
+        self.assertEqual(f2.get_datum(), "hello")
+
+    def test_pass_foo_by_transfer_ptr(self):
+        obj = foo.SomeObject("")
+        f = foo.Foo("hello")
+        obj.set_foo_ptr(f)
+        del f
+        f2 = obj.get_foo_ptr()
+        self.assertEqual(f2.get_datum(), "hello")
+        
+    def test_pass_foo_shared_ptr(self):
+        obj = foo.SomeObject("")
+        f = foo.Foo("hello")
+        obj.set_foo_shared_ptr(f)
+        self.assertEqual(f.get_datum(), "hello")
+        f2 = obj.get_foo_shared_ptr()
+        self.assertEqual(f2.get_datum(), "hello")
+    
+        
+        
+if __name__ == '__main__':
+    unittest.main()
