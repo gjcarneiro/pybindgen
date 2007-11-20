aca1812834ab95d572616890ab78b7bca98bbb39
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2007-11-20 16:46:06 +0000

    Scan class methods and attributes

--- pybindgen/gccxmlparser.py
+++ pybindgen/gccxmlparser.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os.path
+import warnings
 from pygccxml import parser
 from pygccxml import declarations
 from module import Module
@@ -9,9 +10,14 @@ from typehandlers.codesink import FileCodeSink
 from typehandlers.base import ReturnValue, Parameter
 from enum import Enum
 from function import Function
-from cppclass import CppClass
+from cppclass import CppClass, CppConstructor, CppMethod
 from pygccxml.declarations import type_traits
 from pygccxml.declarations import cpptypes
+from pygccxml.declarations import calldef
+
+#from pygccxml.declarations.calldef import \
+#    destructor_t, constructor_t, member_function_t
+from pygccxml.declarations.variable import variable_t
 
 __all__ = ['ModuleScanner']
 
@@ -168,40 +174,172 @@ class ModuleParser(object):
         return module
 
     def _scan_namespace_types(self, module, module_namespace):
+        ## scan enumerations
+        for enum in module_namespace.enums(function=self.location_filter, recursive=False, allow_empty=True):
+            if enum.name.startswith('__'):
+                continue
+            module.add_enum(Enum(enum.name, [name for name, dummy_val in enum.values]))
+
+        ## scan classes
+        unregistered_classes = [cls for cls in
+                                module_namespace.classes(function=self.location_filter,
+                                                         recursive=False, allow_empty=True)
+                                if not cls.name.startswith('__')]
+        registered_classes = {} # class_t -> CppClass
+        while unregistered_classes:
+            cls = unregistered_classes.pop(0)
+            if '<' in cls.name:
+                warnings.warn("Class %s ignored because it is templated; templates not yet supported"
+                              % cls.decl_string)
+                continue
+                
+            if len(cls.bases) > 1:
+                warnings.warn("Class %s ignored because it uses multiple "
+                              "inheritance (not yet supported by pybindgen)"
+                              % cls.decl_string)
+                continue
+            if cls.bases:
+                base_cls = cls.bases[0].related_class
+                try:
+                    base_class_wrapper = registered_classes[base_cls]
+                except KeyError:
+                    ## base class not yet registered => postpone this class registration
+                    if base_cls not in unregistered_classes:
+                        warnings.warn("Class %s ignored because it uses has a base class (%s) "
+                                      "which is not declared."
+                                      % (cls.decl_string, base_cls.decl_string))
+                        continue
+                    unregistered_classes.append(cls)
+                    continue
+            else:
+                base_class_wrapper = None
+
+            if self._class_has_virtual_methods(cls):
+                allow_subclassing = True
+            else:
+                allow_subclassing = None
+
+            if self._class_has_public_destructor(cls):
+                is_singleton = None
+            else:
+                is_singleton = True
+
+            class_wrapper = CppClass(cls.name, parent=base_class_wrapper,
+                                     allow_subclassing=allow_subclassing,
+                                     is_singleton=is_singleton)
+            module.add_class(class_wrapper)
+            registered_classes[cls] = class_wrapper
+            type_registry.register_class(class_wrapper)
+            assert cls.decl_string in type_registry.classes\
+                and type_registry.classes[cls.decl_string] == class_wrapper
+
+        for cls, class_wrapper in registered_classes.iteritems():
+            self._scan_methods(cls, class_wrapper)
+            
+        ## scan nested namespaces (mapped as python submodules)
         for nested_namespace in module_namespace.namespaces(allow_empty=True, recursive=False):
+            if nested_namespace.name.startswith('__'):
+                continue
             nested_module = Module(name=nested_namespace.name, parent=module, cpp_namespace=nested_namespace.name)
             self._scan_namespace_types(nested_module, nested_namespace)
 
-        for enum in module_namespace.enums(function=self.location_filter, recursive=False, allow_empty=True):
-            module.add_enum(Enum(enum.name, [name for name, dummy_val in enum.values]))
+    def _class_has_virtual_methods(self, cls):
+        """return True if cls has at least one virtual method, else False"""
+        for member in cls.get_members('public'):
+            if isinstance(member, calldef.member_function_t):
+                if member.virtuality != calldef.VIRTUALITY_TYPES.NOT_VIRTUAL:
+                    return True
+        return False
 
-        for cls in module_namespace.classes(function=self.location_filter, recursive=False, allow_empty=True):
-            class_wrapper = CppClass(cls.name)
-            module.add_class(class_wrapper)
-            if __debug__:
-                if class_wrapper.full_name.startswith('::'):
-                    full_name = class_wrapper.full_name
+    def _class_has_public_destructor(self, cls):
+        """return True if cls has a public destructor, else False"""
+        for member in cls.get_members('public'):
+            if isinstance(member, calldef.destructor_t):
+                return True
+        return False
+
+    def _scan_methods(self, cls, class_wrapper):
+        for member in cls.get_members('public'):
+
+            if isinstance(member, calldef.member_function_t):
+                try:
+                    return_type = type_registry.lookup_return(member.return_type)
+                except (TypeError, KeyError), ex:
+                    warnings.warn("Return value '%s' error (used in %s): %r"
+                                  % (member.return_type.decl_string, member, ex))
+                    continue
+                arguments = []
+                for arg in member.arguments:
+                    try:
+                        arguments.append(type_registry.lookup_parameter(arg.type, arg.name))
+                    except (TypeError, KeyError), ex:
+                        warnings.warn("Parameter '%s %s' error (used in %s): %r"
+                                      % (arg.type.decl_string, arg.name, member, ex))
+                        ok = False
+                        break
                 else:
-                    full_name = '::'+class_wrapper.full_name
-                assert cls.decl_string == full_name
-            type_registry.register_class(class_wrapper)
-            
+                    ok = True
+                if not ok:
+                    continue
 
+                method_wrapper = CppMethod(return_type, member.name, arguments,
+                                           is_const=member.has_const,
+                                           is_static=member.has_static,
+                                           is_virtual=(member.virtuality !=
+                                                       calldef.VIRTUALITY_TYPES.NOT_VIRTUAL))
+                class_wrapper.add_method(method_wrapper)
+
+            elif isinstance(member, calldef.constructor_t):
+                arguments = []
+                for arg in member.arguments:
+                    try:
+                        arguments.append(type_registry.lookup_parameter(arg.type, arg.name))
+                    except (TypeError, KeyError), ex:
+                        warnings.warn("Parameter '%s %s' error (used in %s): %r"
+                                      % (arg.type.decl_string, arg.name, member, ex))
+                        ok = False
+                        break
+                else:
+                    ok = True
+                if not ok:
+                    continue
+                constructor_wrapper = CppConstructor(arguments)
+                class_wrapper.add_constructor(constructor_wrapper)
+
+            elif isinstance(member, variable_t):
+                try:
+                    return_type = type_registry.lookup_return(member.type)
+                except (TypeError, KeyError), ex:
+                    warnings.warn("Return value '%s' error (used in %s): %r"
+                                  % (member.type.decl_string, member, ex))
+                    continue
+                if member.type_qualifiers.has_static:
+                    class_wrapper.add_static_attribute(return_type, member.name)
+                else:
+                    class_wrapper.add_instance_attribute(return_type, member.name)
+            
+            elif isinstance(member, calldef.destructor_t):
+                pass
+
+            
     def _scan_namespace_functions(self, module, module_namespace):
-        for fun in module_namespace.free_functions(function=self.location_filter, recursive=False):
+        for fun in module_namespace.free_functions(function=self.location_filter,
+                                                   allow_empty=True, recursive=False):
+            if fun.name.startswith('__'):
+                continue
             try:
                 return_type = type_registry.lookup_return(fun.return_type)
             except (TypeError, KeyError), ex:
-                print >> sys.stderr, ("Return value '%s' error (used in %s): %r"
-                                      % (fun.return_type.decl_string, fun, ex))
+                warnings.warn("Return value '%s' error (used in %s): %r"
+                              % (fun.return_type.decl_string, fun, ex))
                 continue
             arguments = []
             for arg in fun.arguments:
                 try:
                     arguments.append(type_registry.lookup_parameter(arg.type, arg.name))
                 except (TypeError, KeyError), ex:
-                    print >> sys.stderr, ("Parameter '%s %s' error (used in %s): %r"
-                                          % (arg.type.decl_string, arg.name, fun, ex))
+                    warnings.warn("Parameter '%s %s' error (used in %s): %r"
+                                  % (arg.type.decl_string, arg.name, fun, ex))
                     ok = False
                     break
             else:
@@ -211,16 +349,24 @@ class ModuleParser(object):
                     
             func_wrapper = Function(return_type, fun.name, arguments)
             module.add_function(func_wrapper)
+
+        ## scan nested namespaces (mapped as python submodules)
+        for nested_namespace in module_namespace.namespaces(allow_empty=True, recursive=False):
+            if nested_namespace.name.startswith('__'):
+                continue
+            nested_module = Module(name=nested_namespace.name, parent=module, cpp_namespace=nested_namespace.name)
+            self._scan_namespace_functions(nested_module, nested_namespace)
     
 
 def _test():
     module_parser = ModuleParser('foo', '::')
     module = module_parser.parse(sys.argv[1:])
-    print "------------ cut here ----------------------"
-    out = FileCodeSink(sys.stdout)
-    import utils
-    utils.write_preamble(out)
-    module.generate(out)
+    if 0:
+        print "------------ cut here ----------------------"
+        out = FileCodeSink(sys.stdout)
+        import utils
+        utils.write_preamble(out)
+        module.generate(out)
 
 if __name__ == '__main__':
     _test()
