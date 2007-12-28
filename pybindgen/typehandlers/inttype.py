a7aba40ba0fadc8fb450fa00f61c41eeae9b42c7
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2007-12-28 14:01:03 +0000

    Add some support for int8_t* and uint8_t* parameters.

--- pybindgen/typehandlers/inttype.py
+++ pybindgen/typehandlers/inttype.py
@@ -164,7 +164,7 @@ class UInt8Return(ReturnValue):
             "%s = %s;" % (self.value, tmp_var))
 
     def convert_c_to_python(self, wrapper):
-        wrapper.build_params.add_parameter("i", [self.value], prepend=True)
+        wrapper.build_params.add_parameter("i", ['(int)' + self.value], prepend=True)
 
 
 class UnsignedLongLongParam(Parameter):
@@ -224,3 +224,46 @@ class LongLongReturn(ReturnValue):
 
     def convert_c_to_python(self, wrapper):
         wrapper.build_params.add_parameter("L", [self.value], prepend=True)
+
+
+class Int8PtrParam(Parameter):
+
+    DIRECTIONS = [Parameter.DIRECTION_IN, Parameter.DIRECTION_OUT,
+                  Parameter.DIRECTION_IN|Parameter.DIRECTION_OUT]
+    CTYPES = ['int8_t*']
+    
+    def convert_c_to_python(self, wrapper):
+        if self.direction & self.DIRECTION_IN:
+            wrapper.build_params.add_parameter('b', ['*'+self.value])
+        if self.direction & self.DIRECTION_OUT:
+            wrapper.parse_params.add_parameter("b", [self.value], self.name)
+
+    def convert_python_to_c(self, wrapper):
+        assert self.ctype.endswith('*')
+        name = wrapper.declarations.declare_variable(self.ctype[:-1], self.name)
+        wrapper.call_params.append('&'+name)
+        if self.direction & self.DIRECTION_IN:
+            wrapper.parse_params.add_parameter('b', ['&'+name], self.name)
+        if self.direction & self.DIRECTION_OUT:
+            wrapper.build_params.add_parameter("b", [name])
+
+class UInt8PtrParam(Parameter):
+
+    DIRECTIONS = [Parameter.DIRECTION_IN, Parameter.DIRECTION_OUT,
+                  Parameter.DIRECTION_IN|Parameter.DIRECTION_OUT]
+    CTYPES = ['uint8_t*']
+    
+    def convert_c_to_python(self, wrapper):
+        if self.direction & self.DIRECTION_IN:
+            wrapper.build_params.add_parameter('B', ['*'+self.value])
+        if self.direction & self.DIRECTION_OUT:
+            wrapper.parse_params.add_parameter("B", [self.value], self.name)
+
+    def convert_python_to_c(self, wrapper):
+        assert self.ctype.endswith('*')
+        name = wrapper.declarations.declare_variable(self.ctype[:-1], self.name)
+        wrapper.call_params.append('&'+name)
+        if self.direction & self.DIRECTION_IN:
+            wrapper.parse_params.add_parameter('B', ['&'+name], self.name)
+        if self.direction & self.DIRECTION_OUT:
+            wrapper.build_params.add_parameter("B", [name])
