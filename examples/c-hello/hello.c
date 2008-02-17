beb11b3823cd55c4c76f8ac93b205bc8d7aafc06
Gustavo J. A. M. Carneiro <gjc@gnome.org>
2008-02-17 21:50:15 +0000

    Add a sample C API based on functions operating over structures in a object oriented way; goal is to support this API in the future (not yet done)

--- examples/c-hello/hello.c
+++ examples/c-hello/hello.c
@@ -1,5 +1,7 @@
 #include "hello.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void hello_print_message(const char *message)
 {
@@ -10,3 +12,76 @@ double hello_sum(double x, double y)
 {
     return x + y;
 }
+
+
+struct _HelloFoo
+{
+    int refcount;
+    char *data;
+};
+
+HelloFoo*
+hello_foo_new(void)
+{
+    HelloFoo *foo;
+    foo = (HelloFoo *) malloc(sizeof(HelloFoo));
+    foo->refcount = 1;
+    foo->data = NULL;
+    return foo;
+}
+
+HelloFoo*
+hello_foo_new_from_data(const char *data)
+{
+    HelloFoo* foo;
+
+    foo = hello_foo_new();
+    hello_foo_set_data(foo, data);
+    return foo;
+}
+
+HelloFoo*
+hello_foo_new_with_spaces (int num_spaces)
+{
+    int i;
+    HelloFoo *foo;
+    foo = hello_foo_new();
+    foo->data = malloc(num_spaces + 1);
+    for (i = 0; i < num_spaces; i++)
+	foo->data[i] = ' ';
+    foo->data[i] = '\0';
+    return foo;
+}
+
+void
+hello_foo_ref(HelloFoo *foo)
+{
+    foo->refcount++;
+}
+
+void
+hello_foo_unref(HelloFoo *foo)
+{
+    if (--foo->refcount > 0)
+        return;
+    
+    if (foo->data)
+	    free(foo->data);
+    free(foo);
+}
+
+void
+hello_foo_set_data(HelloFoo   *foo,
+		   const char *data)
+{
+    if (foo->data)
+	free(foo->data);
+    foo->data = strdup(data);
+}
+
+const char *
+hello_foo_get_data(HelloFoo *foo)
+{
+    return foo->data;
+}
+
--- examples/c-hello/hello.h
+++ examples/c-hello/hello.h
@@ -7,8 +7,23 @@ extern "C"
 #endif
 
 
-void hello_print_message(const char *message);
-double hello_sum(double x, double y);
+void   hello_print_message (const char *message);
+double hello_sum           (double      x,
+                            double      y);
+
+
+typedef struct _HelloFoo HelloFoo;
+
+
+HelloFoo*    hello_foo_new             (void);
+HelloFoo*    hello_foo_new_from_data   (const char *data);
+HelloFoo*    hello_foo_new_with_spaces (int         num_spaces);
+void         hello_foo_ref             (HelloFoo   *foo);
+void         hello_foo_unref           (HelloFoo   *foo);
+void         hello_foo_set_data        (HelloFoo   *foo,
+                                        const char *data);
+const char * hello_foo_get_data        (HelloFoo   *foo);
+
 
 
 #ifdef __cplusplus
