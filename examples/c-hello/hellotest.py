bd11af0076a35ef6d6b1195426843ff60103644b
Gustavo J. A. M. Carneiro <gjc@gnome.org>
2008-03-02 19:36:36 +0000

    fix the unit tests

--- examples/c-hello/hellotest.py
+++ examples/c-hello/hellotest.py
@@ -16,7 +16,23 @@ class TestHello(unittest.TestCase):
     def test_foo(self):
         foo = hello.Foo()
         data = foo.get_data()
-        self.assertEqual(data, '')
+        self.assertEqual(data, None)
+        del foo
+        while gc.collect():
+            pass
+
+    def test_foo2(self):
+        foo = hello.Foo("123")
+        data = foo.get_data()
+        self.assertEqual(data, "123")
+        del foo
+        while gc.collect():
+            pass
+
+    def test_foo3(self):
+        foo = hello.Foo(3)
+        data = foo.get_data()
+        self.assertEqual(data, "   ")
         del foo
         while gc.collect():
             pass
