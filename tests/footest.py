0148bbf3fe0038fe20798fa9db00aff19157e012
Gustavo J. A. M. Carneiro <gjc@gnome.org>
2008-05-01 19:56:51 +0100

    Fix the built test modules path (waf check was testing installed modules!)

--- tests/footest.py
+++ tests/footest.py
@@ -3,7 +3,7 @@ import weakref
 import gc
 import os.path
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                '..', 'build', 'default', 'examples'))
+                                '..', 'build', 'default', 'tests'))
 if len(sys.argv) > 1:
     import foo2 as foo
     del sys.argv[1]
