5a4317a22f84355de98a09bba408bfba6d895507
Gustavo J. A. M. Carneiro <gjc@gnome.org>
2009-02-18 12:15:22 +0000

    Add wrapping of std::ofstream to the example

--- examples/g/modulegen.py
+++ examples/g/modulegen.py
@@ -15,6 +15,21 @@ def my_module_gen(out_file):
     GInner = G.add_cpp_namespace("GInner")
     GInner.add_function('GDoC', None, [])
 
+    G.add_include('<fstream>')
+
+    ofstream = G.add_class('ofstream', foreign_cpp_namespace='::std')
+    ofstream.add_enum('openmode', [
+            ('app', 'std::ios_base::app'),
+            ('ate', 'std::ios_base::ate'),
+            ('binary', 'std::ios_base::binary'),
+            ('in', 'std::ios_base::in'),
+            ('out', 'std::ios_base::out'),
+            ('trunc', 'std::ios_base::trunc'),
+            ])
+    ofstream.add_constructor([Parameter.new("const char *", 'filename'),
+                              Parameter.new("::std::ofstream::openmode", 'mode', default_value="std::ios_base::out")])
+    ofstream.add_method('close', None, [])
+
     mod.generate(FileCodeSink(out_file))
 
 if __name__ == '__main__':
