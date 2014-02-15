3efed70d0e77a830bb3fbfc3e581739f7ec0aa32
Gustavo Carneiro <gjcarneiro@gmail.com>
2014-02-15 18:31:41 +0000

    Update setup.py manifest file

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import sys
 import os
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-from wutils import get_version, generate_version_py
-
 from distutils.core import setup
 
+sys.path.insert(0, os.path.dirname(__file__))
+from wutils import get_version, generate_version_py
 generate_version_py(force=False)
 
 setup(name='pybindgen',
