871990a7a41d0d1dd0951cc20710bd85f36d522d
Gustavo Carneiro <gjcarneiro@gmail.com>
2014-02-16 22:37:01 +0000

    Convert the README to restructured text format instead of markdown, and upload it to PyPI as long_description.

--- setup.py
+++ setup.py
@@ -7,12 +7,17 @@ sys.path.insert(0, os.path.dirname(__file__))
 from wutils import get_version, generate_version_py
 generate_version_py(force=False)
 
+with open('README') as file_:
+    long_description = file_.read()
+
+
 setup(name='PyBindGen',
-      version=get_version(),
+      version='0.17.0', #get_version(),
       description='Python Bindings Generator',
       author='Gustavo Carneiro',
       author_email='gjcarneiro@gmail.com',
       url='https://launchpad.net/pybindgen',
       packages=['pybindgen', 'pybindgen.typehandlers', 'pybindgen.typehandlers.ctypeparser'],
+      long_description=long_description
      )
 
